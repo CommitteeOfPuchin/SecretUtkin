22ce089247c0b4054f3618d5ac7d75fe1548f21f
atik1n <chizhovnikita2000@gmail.com>
2019-02-19 21:49:00 +0300

    Зайчатки взаимодействий клиент-сервер

--- server.py
+++ server.py
@@ -2,30 +2,110 @@ import asyncio
 import websockets
 import time
 import base64
+from aioconsole import ainput
 
+wss = {}
 
 async def wakeup():
 	while True:
 		await asyncio.sleep(1)
 
+async def con():
+	while True:
+		line = await ainput()
+		line = line.split()
+		
+		with open("Avatars/%s.png" % line[1], "rb") as image_file:
+			encoded_string = base64.b64encode(image_file.read())
+		
+		for ws in wss:
+			if ws.closed:
+				print('fuck')
+			await ws.send('user %s' % line[0])
+			await ws.send(encoded_string)
+			print('Server -> %s | %s' % (ws.remote_address[0], line))
+
+
+def avaGen(websocket):
+	data = [a[1] for a in wss.values()]
+	if wss[websocket][1].startswith('K'):
+		data = ['T_Unknown.png' if not x.startswith('K') else x for x in data]
+
+	else:
+		data = ['T_Unknown.png' if x != wss[websocket][1] else x for x in data]
+
+	for i, path in enumerate(data):
+		with open('Cards/%s' % path, "rb") as image_file:
+			data[i] = base64.b64encode(image_file.read())
+	return data
+
 
 async def hello(websocket, path):
 	print('%s connected' % websocket.remote_address[0])
-	while True:
-		t = input().split()
+	
+	if len(wss) == 10:
+		websocket.close()
+		return
+		
+	wss[websocket] = (await websocket.recv(), "T_Unknown.png")
+	print(wss)
+		
+	await websocket.send('hs.default')
+	await asyncio.sleep(0.1)
+	await websocket.send(T_Placeholder)
+	await asyncio.sleep(0.1)
+	print(1)
 
-		with open("Avatars/%s.png" % t[1], "rb") as image_file:
-			encoded_string = base64.b64encode(image_file.read())
+	await websocket.send('hs.you')
+	await asyncio.sleep(0.1)
+	await websocket.send(str(list(wss.keys()).index(websocket)))
 
-		await websocket.send('user %s' % t[0])
-		await websocket.send(encoded_string)
-		print('%s | %s' % (websocket.remote_address[0], t))
+	await websocket.send('hs.players')
+	await asyncio.sleep(0.1)
+	await websocket.send(repr([a[0] for a in wss.values()]))
+	await asyncio.sleep(0.1)
+	await websocket.send(repr(avaGen(websocket)))
 
+	for ws in wss.keys():
+		if ws == websocket:
+			continue
+
+		await ws.send('player.join')
+		await ws.send(repr([wss[websocket][0], T_Unknown]))
+		await asyncio.sleep(0.1)
+	print(2)
+
+	closed = False
+	while not closed:
+		await websocket.wait_closed()
+		closed = websocket.closed
+
+	print('%s disconnected' % websocket.remote_address[0])
+	print('Reason: %s' % websocket.close_reason)
+
+	for ws in wss.keys():
+		if ws == websocket:
+			continue
+		await ws.send('player.leave')
+		await asyncio.sleep(0.1)
+		await ws.send(str(list(wss.keys()).index(websocket)))
+	print(3)
+
+	del wss[websocket]
+
+	print(wss)
 
 start_server = websockets.serve(hello, '0.0.0.0', 6999)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().create_task(wakeup())
+asyncio.get_event_loop().create_task(con())
+
+with open('Cards/T_Placeholder.png', "rb") as image_file:
+	T_Placeholder = base64.b64encode(image_file.read())
+
+with open('Cards/T_Unknown.png', "rb") as image_file:
+	T_Unknown = base64.b64encode(image_file.read())
+
 try:
 	asyncio.get_event_loop().run_forever()
 except KeyboardInterrupt:
--- test.py
+++ test.py
@@ -1,58 +1,107 @@
 import asyncio
 import websockets
 import threading
-import time
+import time, sys
+import queue
+import ast
 from itertools import cycle
 import tkinter as tk
 from tkinter import font as tkfont
-from tkinter import filedialog
 
-# def _asyncio_thread(loop, text):
-#     async def send():
-#         async with websockets.connect(
-#                 'ws://salieri.me:6999') as websocket:
-#             await websocket.send(text)
-#             print(await websocket.recv())
-#
-#     loop.run_until_complete(send())
-#
-#
-# def SendMes(ev):
-#     loop = asyncio.get_event_loop()
-#     t = threading.Thread(target=_asyncio_thread, args=(loop,textbox.get('1.0', 'end'),)).start()
 
 class Net:
-    @staticmethod
-    def init(frame):
-        async def wakeup():
-            while True:
-                await asyncio.sleep(1)
+    def __init__(self, address, nickname, frame=None):
+        self.address = address
+        if ':' not in address:
+            self.address = '%s:6999' % self.address
 
-        loop = asyncio.get_event_loop()
-        loop.create_task(wakeup())
-        threading.Thread(target=Net._asyncio_thread, args=(loop, frame)).start()
+        self.defaultIcon = None
+        self.nick = nickname
+        self.frame = frame
+        self.players = []
+        self.avatars = []
+        self.format = [
+            [2, ],
+            [2, 7],
+            [1, 2, 3],
+            [1, 2, 3, 7],
+            [1, 6, 2, 3, 8],
+            [1, 2, 3, 6, 7, 8],
+            [0, 1, 2, 3, 4, 6, 8],
+            [0, 1, 2, 3, 4, 6, 7, 8],
+            [0, 1, 2, 3, 4, 5, 7, 8, 9],
+            range(10)
+        ]
+        self.closed = False
+        self.thread = None
+        self.loop = asyncio.get_event_loop()
+        self.connect_status = 0
+        self.tasks = []
 
-    @staticmethod
-    def _asyncio_thread(loop, frame):
+    def set_frame(self, frame):
+        self.frame = frame
+
+    def connect(self):
+        self.thread = threading.Thread(target=Net._asyncio_thread, args=(self, self.loop))
+        self.thread.start()
+
+    def _asyncio_thread(self, loop):
         async def send():
-            async with websockets.connect(
-              'ws://salieri.me:6999') as websocket:
-                while True:
-                    cmd = await websocket.recv()
-                    print(cmd)
+            try:
+                async with websockets.connect(
+                  'ws://%s' % self.address) as websocket:
+                    self.connect_status = 1
 
-                    print(cmd.startswith('user'))
+                    await websocket.send(self.nick)
 
-                    if cmd.startswith('user'):
-                        args = cmd.split()[1:]
-                        print(args)
-                        avaFrame = frame.avatars[int(args[0])]
-                        avaFrame.avatar = tk.PhotoImage(data=await websocket.recv())
-                        avaFrame.avaLabel.configure(image=avaFrame.avatar)
+                    while not websocket.closed:
+                        try:
+                            cmd = await websocket.recv()
+                        except asyncio.CancelledError:
+                            print('oof')
+                            await websocket.close(reason='hyu')
+                            raise
 
+                        print(cmd)
+
+                        if cmd == 'hs.players':
+                            self.players = ast.literal_eval(await websocket.recv())
+                            self.avatars = ast.literal_eval(await websocket.recv())
+                            self.frame.updatePlayers(self.format, self.avatars, self.players)
+
+                        if cmd == 'hs.default':
+                            dat = await websocket.recv()
+                            self.frame.placeholder = tk.PhotoImage(data=dat)
+                            self.frame.placeholders()
+
+                        if cmd == 'hs.you':
+                            self.frame.playerid = int(await websocket.recv())
+                            print(self.frame.playerid)
+
+                        if cmd == 'player.join':
+                            dat = ast.literal_eval(await websocket.recv())
+                            print(dat)
+                            self.players.append(dat[0])
+                            self.avatars.append(dat[1])
+                            self.frame.updatePlayers(self.format, self.avatars, self.players)
+
+                        if cmd == 'player.leave':
+                            dat = int(await websocket.recv())
+                            del self.players[dat]
+                            del self.avatars[dat]
+                            if self.frame.playerid > dat:
+                                self.frame.playerid -= 1
+                            self.frame.updatePlayers(self.format, self.avatars, self.players)
+
+            except asyncio.CancelledError:
+                print('oof')
+                raise
+            else:
+                self.connect_status = -1
 
         try:
-            loop.run_until_complete(send())
+            self.tasks.append(loop.create_task(send()))
+            loop.run_forever()
         except KeyboardInterrupt:
             pass
 
@@ -64,66 +113,72 @@ class MainWindow(tk.Tk):
         self.geometry("1280x720")
         self.resizable(0, 0)
 
-        container = tk.Frame(bg='#36393f')
-        container.pack(fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container = tk.Frame(bg='#36393f')
+        self.container.place(relheight=1, relwidth=1, relx=0.5, rely=0.5, anchor='c')
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.net = None
 
         self.title("Secret Utkin || Test")
 
         self.frames = {}
-        for F in (StartPage, GamePage):
+        for F in (MainPage, GamePage, ClosingPage):
             page_name = F.__name__
-            frame = F(parent=container, controller=self)
+            frame = F(parent=self.container, controller=self)
             self.frames[page_name] = frame
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame("StartPage")
-        Net.init(self.frames["StartPage"])
+        self.show_frame("MainPage")
 
     def show_frame(self, page_name):
+        for f in self.frames.values():
+            f.grid_forget()
+
         frame = self.frames[page_name]
-        frame.tkraise()
+        frame.grid(row=0, column=0)
+        return frame
 
 
-class StartPage(tk.Frame):
+class GamePage(tk.Frame):
+    def updatePlayers(self, f, images, names):
+        self.placeholders()
+
+        fmt = f[len(names)-1]
+        for i in range(len(names)):
+            a = self.avatars[fmt[i]]
+            a.avatar = tk.PhotoImage(data=images[i])
+            a.nickname = names[i]
+            a.upd()
+
+            if i == self.playerid:
+                a.changeStatus('player')
+
+    def placeholders(self):
+        for ava in self.avatars:
+            ava.avatar = self.placeholder
+            ava.avaLabel.configure(image=self.placeholder)
+            ava.nickLabel.configure(text="Свободно")
+            ava.changeStatus('alive')
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.ready = False
         self.controller = controller
+        self.playerid = 0
 
         self.configure(bg='#36393f')
 
-        frame = tk.Frame(bg='#36393f')
-        frame.place(in_=parent, anchor="c", relx=.5, rely=.5)
-
-        self.avatars = []
-        for _ in range(10):
-            self.avatars.append(None)
-
-        self.avatars[0] = Avatar(frame, tk.PhotoImage(file="Avatars/K_Vilvi.png"), "vilverin", status="player")
-        self.avatars[1] = Avatar(frame, tk.PhotoImage(file="Avatars/L_Nikminer.png"), "Nikminer")
-        self.avatars[2] = Avatar(frame, tk.PhotoImage(file="Avatars/L_Risa.png"), "risa-chan")
-        self.avatars[3] = Avatar(frame, tk.PhotoImage(file="Avatars/K_Mjaroslav.png"), "mjaroslav", status='alpaca')
-        self.avatars[4] = Avatar(frame, tk.PhotoImage(file="Avatars/L_Enotello.png"), "enotello", status='clear')
-        self.avatars[5] = Avatar(frame, tk.PhotoImage(file="Avatars/L_Raven.png"), "Raven")
-        self.avatars[6] = Avatar(frame, tk.PhotoImage(file="Avatars/U_Utkin.png"), "Utkin")
-        self.avatars[7] = Avatar(frame, tk.PhotoImage(file="Avatars/L_ElvenMeat.png"), "ElvenMeat")
-        self.avatars[8] = Avatar(frame, tk.PhotoImage(file="Avatars/K_Ryoka.png"), "Ryoka", status='clear')
-        self.avatars[9] = Avatar(frame, tk.PhotoImage(file="Avatars/L_Event.png"), "eventhorizon")
-
-        self.avatars[2].changeStatus('alive', True)
-        self.avatars[8].changeStatus('alive', False, True)
+        self.placeholder = tk.PhotoImage()
+        self.avatars = [Avatar(self, self.placeholder, "") for _ in range(10)]
 
         self.factions = ['#0067bf', '#97612b', '#bfa200', 'black']
 
         for i, avatar in enumerate(self.avatars):
-            avatar.grid(row=(i//5)+1, column=(i % 5)+1, padx=10, pady=10)
+            avatar.grid(row=(i//5)+1, column=(i % 5)+1, padx=10, pady=5)
             avatar.avaLabel.bind("<Button-1>", lambda _, s=avatar.nickname: print(s))
             avatar.nickLabel.bind("<Button-1>", lambda _, col=cycle(self.factions), a=avatar: a.nickLabel.configure(fg=next(col)))
+
+        self.ready = True
 
 
 class Avatar(tk.Frame):
@@ -134,7 +189,7 @@ class Avatar(tk.Frame):
 
         self.status = status
         self.colors = {
-            'alive': None,
+            'alive': "#f0f0f0",
             'alpaca': 'gray',
             'player': 'green',
             'clear': '#92b6ef'
@@ -161,13 +216,105 @@ class Avatar(tk.Frame):
         else:
             self.configure(highlightbackground="black")
 
+    def upd(self):
+        self.avaLabel.configure(image=self.avatar)
+        self.nickLabel.configure(text=self.nickname)
 
-class GamePage(tk.Frame):
+
+class MainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.bg_col = '#36393f'
+
+        self.configure(bg=self.bg_col, borderwidth=2, relief='solid')
+        self.e1 = tk.Entry(self, font='Helvetica 10', width=20, fg='grey')
+        self.e2 = tk.Entry(self, font='Helvetica 10', width=20, fg='grey')
+
+        def_text = ['test', 'test2']
+        self.def_e = {}
+
+        for i, e in enumerate((self.e1, self.e2)):
+            e.insert(0, def_text[i])
+            self.def_e[e] = def_text[i]
+            e.bind('<FocusIn>', self.entry_focus_in)
+            e.bind('<FocusOut>', self.entry_focus_out)
+
+        self.e1.grid(row=0, column=1, padx=5)
+        self.e2.grid(row=1, column=1, padx=5)
+
+        l1 = tk.Label(self, text='Никнейм', bg=self.bg_col, font='Helvetica 10 bold', fg='white')
+        l1.grid(row=0, column=0, sticky='e', padx=5, pady=5)
+
+        l2 = tk.Label(self, text='IP', bg=self.bg_col, font='Helvetica 10 bold', fg='white')
+        l2.grid(row=1, column=0, sticky='e', padx=5, pady=5)
+
+        self.b = tk.Button(self, text="Войти", font='Helvetica 12', width=20)
+        self.b.grid(row=2, column=0, columnspan=2, pady=10)
+        self.b.bind('<Button-1>', self.login)
+
+    def login(self, event):
+        if event.widget['state'] == 'normal':
+            nick = 'Anon'
+
+            if self.e1.get() != '':
+                nick = self.e1.get()
+
+            if self.e2.get() != '':
+                self.controller.net = Net(self.e2.get(), nick)
+                self.controller.net.connect()
+
+                while not self.controller.net.connect_status:
+                    pass
+
+                if self.controller.net.connect_status == 1:
+                    print('nice')
+                    self.controller.show_frame("GamePage")
+                    self.controller.net.frame = self.controller.frames["GamePage"]
+                else:
+                    print('whoops')
+                    self.b.configure(text='Упс...')
+                    self.controller.net.connect_status = 0
+
+    def entry_focus_in(self, event):
+        if event.widget.get() == self.def_e[event.widget]:
+            event.widget.configure(fg='black')
+            event.widget.delete(0, 'end')
+            event.widget.insert(0, '')
+
+    def entry_focus_out(self, event):
+        if event.widget.get() == '':
+            event.widget.insert(0, self.def_e[event.widget])
+            event.widget.configure(fg='grey')
+
+
+class ClosingPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        self.configure(bg='#36393f')
+
+        label = tk.Label(self, text='Подождите, закрывается...', font='Helvetica 24', bg='black', fg='white')
+        label.pack()
+
+
+def on_closing():
+    app.wait_visibility(app.show_frame('ClosingPage'))
+
+    loop = asyncio.get_event_loop()
+    loop.stop()
+
+    pending = asyncio.Task.all_tasks(loop=loop)
+    gathered = asyncio.gather(*pending, loop=loop)
+    gathered.cancel()
+
+    sys.exit(0)
+
+
 
 if __name__ == "__main__":
     app = MainWindow()
+    flags = []
+    app.protocol("WM_DELETE_WINDOW", on_closing)
     app.mainloop()
